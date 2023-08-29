e503586e6df2617f79d3eff9f4d87be0c67f7ea0
arinrdz220222 <6410210688@psu.ac.th>
2023-08-29 14:53:04 +0700

    exam1

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,83 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.support.Repositories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+
+@RestController
+public class StudentController {
+	
+	@Autowired
+	StudentRepository studentRepository;
+	
+	@GetMapping("/students")
+	public ResponseEntity<Object> getStudents() {
+		try {
+			List<Student> students = studentRepository.findAll();
+			return new ResponseEntity<>(students, HttpStatus.OK);
+
+		} catch (Exception e) {
+			return new ResponseEntity<>("Internal Sever Error", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+	
+	@PostMapping("/students")
+	public ResponseEntity<Object> addStudent(@RequestBody Student body) {
+		try {
+			Student student = studentRepository.save(body);
+			return new ResponseEntity<> (student, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@PutMapping("/students/{studentId}")
+	public ResponseEntity<Object> updatStudents(@PathVariable Integer id, @RequestBody Student body) {
+		Optional<Student> student = studentRepository.findById(id);
+
+		try {
+			if (student.isPresent()) {
+				student.get().setFirstName(body.getFirstName());
+				student.get().setSurName(body.getSurName());
+				student.get().setEmail(body.getEmail());
+				studentRepository.save(student.get());
+				return new ResponseEntity<>(student.get(), HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>("Student not found", HttpStatus.BAD_REQUEST);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<>("Internal Sever Error", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@DeleteMapping("/students/{id}")
+	public ResponseEntity<Object> deleteStudents(@PathVariable Integer id) {
+		Optional<Student> student = studentRepository.findById(id);
+		try {
+			if (student.isPresent()) {
+				studentRepository.delete(student.get());
+				return new ResponseEntity<>("Deleted successful", HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>("Student not found", HttpStatus.BAD_REQUEST);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<>("Internal Sever Error", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+}
---
+++ src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,58 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Student")
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer studentId;
+	private String stdId;
+	private String firstName;
+	private String surName;
+	private String email;
+	public Student(Integer studentId, String stdId, String firstName, String surName, String email) {
+		super();
+		this.studentId = studentId;
+		this.stdId = stdId;
+		this.firstName = firstName;
+		this.surName = surName;
+		this.email = email;
+	}
+	public Integer getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+	public String getStdId() {
+		return stdId;
+	}
+	public void setStdId(String stdId) {
+		this.stdId = stdId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getSurName() {
+		return surName;
+	}
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}	
+	
+}
